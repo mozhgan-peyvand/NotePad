7905be9078b76876e8056dd5a58253dc82a0c04e
mozhgan <mozhepeivnd@gmail.com>
2020-04-23 01:45:52 -0700

    Create best adapter (#3)

--- app/src/main/java/com/example/notepad/features/home/ui/NoteListAdapter.kt
+++ app/src/main/java/com/example/notepad/features/home/ui/NoteListAdapter.kt
@@ -1,5 +1,67 @@
 package com.example.notepad.features.home.ui
 
-class NoteListAdapter {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableList
+import androidx.recyclerview.widget.DiffUtil
+import com.example.notepad.R
+import com.example.notepad.core.ui.safeNavigate
+import com.example.notepad.core.util.binding.DataBoundListAdapter
+import com.example.notepad.databinding.ItemNoteListBinding
+
+class NoteListAdapter (private val context: Context?):
+    DataBoundListAdapter<NoteItemInfoView, ItemNoteListBinding>(
+        diffCallback = object : DiffUtil.ItemCallback<NoteItemInfoView>() {
+            override fun areItemsTheSame(
+                oldItem: NoteItemInfoView,
+                newItem: NoteItemInfoView
+            ): Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(
+                oldItem: NoteItemInfoView,
+                newItem: NoteItemInfoView
+            ): Boolean {
+                return oldItem.title == newItem.title
+            }
+        }
+    ) {
+//    private val context: Context
+
+    override fun createBinding(parent: ViewGroup): ItemNoteListBinding {
+        return DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_note_list,
+            parent,
+            false
+        )
+    }
+
+    @SuppressLint("ShowToast", "LogNotTimber")
+    override fun bind(binding: ItemNoteListBinding, item: NoteItemInfoView, position: Int) {
+        binding.apply {
+
+            noteItem = item
+            root.setOnClickListener {
+                noteItem.let {
+                    Log.d("you click on me","yes im work")
+                }
+            }
+
+        }
+    }
+
+    private var noteItemInfoView: MutableList<NoteItemInfoView>? = null
+
+    override fun submitList(list: MutableList<NoteItemInfoView>?) {
+        noteItemInfoView = list
+        super.submitList(list)
+    }
 
 }
--- app/src/main/java/com/example/notepad/features/note/ui/NoteFragment.kt
+++ app/src/main/java/com/example/notepad/features/note/ui/NoteFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.example.notepad.R
+import com.example.notepad.core.util.autoCleared
+import com.example.notepad.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
 
+    private var binding by autoCleared<FragmentNoteBinding>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false)
+         binding =DataBindingUtil.inflate(inflater,R.layout.fragment_note, container, false)
+        return binding.root
     }
 
 
